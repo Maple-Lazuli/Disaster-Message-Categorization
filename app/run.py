e7eddad3e37708579986b1ecee41a305f7057f2c
Maple-Lazuli <ada.lazuli@spaceforce.mil>
2021-03-29 01:31:58 -0400

    finshed sources tab of the app

--- app/run.py
+++ app/run.py
@@ -1,38 +1,41 @@
 import json
 import plotly
 import pandas as pd
-
+import joblib
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request
 from plotly.graph_objs import Bar, Figure, Table
-import joblib
 from sqlalchemy import create_engine
-
 app = Flask(__name__)
+"""
+Summary of Routes:
+    1. / and /index - Both return index.html and present the form to submit a message to query
+    2. /go - return go.html which contains the form to submit a message as well as the previous classification results
+    3. /overview - returns overview.html and provides information on the project, dataset, and model
+    4. /sources - returns sources.html and provides references and acknowledgements.
+"""
 
 
 def tokenize(text):
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
-
     clean_tokens = []
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-
     return clean_tokens
+
+
+def generate_df_sums(input_df):
+    dataframe = input_df.drop(['id', 'message', "original", 'genre'], axis=1)
+    return pd.DataFrame({'Category': dataframe.sum(axis=0).index, 'Occurrences': dataframe.sum(axis=0).values})
 
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('messages', engine)
-
-def generate_df_sums(df):
-    dataframe = df.drop(['id', 'message', "original", 'genre'], axis=1)
-    return pd.DataFrame({'Category': dataframe.sum(axis=0).index, 'Occurrences': dataframe.sum(axis=0).values})
-
 
 # load model
 model = joblib.load("../models/classifier.pkl")
@@ -41,24 +44,18 @@ model = joblib.load("../models/classifier.pkl")
 eval_df = pd.read_pickle("../models/eval.pkl")
 
 
-# index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
 
-# web page that handles user query and displays model results
 @app.route('/go')
 def go():
-    # save user input in query
-    query = request.args.get('query', '')
 
-    # use model to predict classification for query
+    query = request.args.get('query', '')
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
-
-    # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
         query=query,
@@ -68,7 +65,6 @@ def go():
 
 @app.route('/overview')
 def overview():
-    # extract data needed for visuals
     cate_sums = generate_df_sums(df)
     dataframe = df.drop(['id', 'genre'], axis=1)
     dataframe = dataframe[0:3]
@@ -83,19 +79,16 @@ def overview():
             align="left")
     )
     ])
-
     table.update_layout(
         autosize=True,
     )
-    # create visuals
-    counts_graph =  {
+    counts_graph = {
             'data': [
                 Bar(
                     x=cate_sums['Category'],
                     y=cate_sums['Occurrences']
                 )
             ],
-
             'layout': {
                 'title': '<b> Occurrences By Category </b>',
                 'yaxis': {
@@ -124,14 +117,14 @@ def overview():
                 }
             }
         }
-    # encode plotly graphs in JSON
-    tableJSON = json.dumps(table, cls=plotly.utils.PlotlyJSONEncoder)
-    countsJSON = json.dumps(counts_graph, cls=plotly.utils.PlotlyJSONEncoder)
-    accuracyJSON = json.dumps(accuracy_graph, cls=plotly.utils.PlotlyJSONEncoder)
-    # render web page with plotly graphs
-    return render_template('overview.html', countsJSON=countsJSON, tableJSON=tableJSON, accuracyJSON=accuracyJSON)
+    table_json = json.dumps(table, cls=plotly.utils.PlotlyJSONEncoder)
+    counts_json = json.dumps(counts_graph, cls=plotly.utils.PlotlyJSONEncoder)
+    accuracy_json = json.dumps(accuracy_graph, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('overview.html', countsJSON=counts_json, tableJSON=table_json, accuracyJSON=accuracy_json)
 
-
+@app.route("/sources")
+def sources():
+    return render_template('sources.html')
 
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
