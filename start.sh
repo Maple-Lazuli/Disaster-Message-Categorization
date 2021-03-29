255c794e0bc7ef91bc04e77322c8b86c1070a7ee
Maple-Lazuli <ada.lazuli@spaceforce.mil>
2021-03-28 21:26:10 -0400

    added a shell script to create the models and start the flask app

---
+++ start.sh
@@ -0,0 +1,5 @@
+source venv/bin/activate
+python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
+python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl
+cd app
+python run.py
